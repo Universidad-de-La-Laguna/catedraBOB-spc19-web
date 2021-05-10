6405aad86359b3580cbe1d2d028a40c5750d4320
krl <alu0100966589@ull.edu.es>
2021-05-10 20:28:31 +0100

    feat: Updated welcome page content
    - Added some information about the prototype
    - Added information about what operations can do each entity

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,63 +1,93 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
-import styles from './Welcome.less';
+import { Card, Typography, List } from 'antd';
+import { useIntl, FormattedMessage, connect, Link } from 'umi';
+import {
+  FormOutlined,
+  HeartFilled,
+  HomeFilled,
+  LockFilled,
+  SendOutlined,
+  UnorderedListOutlined,
+} from '@ant-design/icons';
 
-const CodePreview = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+const data = [
+  {
+    title: 'Insurer',
+    avatar: <LockFilled />,
+    description:
+      'Thanks to the blockchain technology, the insurer can trust the system and pay without needing to know the sensible information of the clients',
+  },
+  {
+    title: 'Hotels',
+    avatar: <HomeFilled />,
+    description:
+      'Can request insurances immediately, easily and transparently so their customers and themselves can feel safer',
+  },
+  {
+    title: 'Laboratories',
+    avatar: <HeartFilled />,
+    description:
+      'Will receive PCR requests automatically and inform their results also in a comfortable manner',
+  },
+];
 
-export default () => {
+const Welcome = ({ currentAuthority }) => {
   const intl = useIntl();
   return (
     <PageContainer>
       <Card>
-        <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: 'Faster and stronger heavy-duty components that have been released.',
-          })}
-          type="success"
-          showIcon
-          banner
-          style={{
-            margin: -12,
-            marginBottom: 24,
-          }}
+        <Typography.Title>Welcome {currentAuthority}</Typography.Title>
+        <Typography.Paragraph>
+          SPC-19 is a spanish acronym that stands for "Parametric Insurance for Covid-19". It is a
+          prototype product made to cover the risk of contracting the SARS-COV-2 virus during a stay
+          in a hotel room. It is based on the Blockchain technology to ensure security, privacy,
+          transparency and trust among all the participating agents:
+        </Typography.Paragraph>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={item.avatar}
+                title={item.title}
+                description={item.description}
+              />
+            </List.Item>
+          )}
         />
-        <Typography.Text strong>
-          <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="Advanced Forms" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/table"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome to use" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-        <Typography.Text
-          strong
-          style={{
-            marginBottom: 12,
-          }}
-        >
-          <FormattedMessage id="pages.welcome.advancedLayout" defaultMessage="Advanced Layout" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/layout"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome to use" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+        <Typography.Paragraph style={{ marginTop: '3rem' }}>
+          As a {currentAuthority} you can:
+        </Typography.Paragraph>
+        <List itemLayout="horizontal" style={{ marginLeft: '1rem' }}>
+          {currentAuthority === 'taker' && (
+            <List.Item.Meta
+              avatar={<FormOutlined />}
+              title={<Link to="/new">Create a new insurance</Link>}
+            />
+          )}
+          {currentAuthority === 'taker' && (
+            <List.Item.Meta
+              avatar={<UnorderedListOutlined />}
+              title={<Link to="/insurances">See all your insurances</Link>}
+            />
+          )}
+          {currentAuthority === 'insurer' && (
+            <List.Item.Meta
+              avatar={<UnorderedListOutlined />}
+              title={<Link to="/insurances">See all insurances</Link>}
+            />
+          )}
+          {currentAuthority === 'lab' && (
+            <List.Item.Meta avatar={<SendOutlined />} title="Set a PCR result" />
+          )}
+        </List>
       </Card>
     </PageContainer>
   );
 };
+
+export default connect(({ login }) => ({
+  currentAuthority: login.currentAuthority,
+}))(Welcome);
